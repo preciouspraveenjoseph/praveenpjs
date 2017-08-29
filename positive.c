b568d92e3bdf51c134d16aa73b00c09a395ed58b
preciouspraveenjoseph <31438445+preciouspraveenjoseph@users.noreply.github.com>
2017-08-29 16:14:15 +0530

    Create positive.c

---
+++ positive.c
@@ -0,0 +1,16 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+ clrscr();
+ int number;
+ cout<<"Enter a number : ";
+ cin>>number;
+ if
+   cout<<"Number is Positive";
+ else if
+   cout<<"Number is Negative";
+ else
+   cout<<"Number is Zero";
+ getch();
+}
